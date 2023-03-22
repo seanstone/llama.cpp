c15b67a56e2af678da55fd8412abb5bbb26fa7c9
En Shih <seanstone5923@gmail.com>
2023-03-22 16:25:38 +0800

    Fix for n_parts of new alpaca-13b file

--- main.cpp
+++ main.cpp
@@ -70,7 +70,7 @@ static const int EOS_TOKEN_ID = 2;
 // determine number of model parts based on the dimension
 static const std::unordered_map<int, int> LLAMA_N_PARTS = {
     { 4096, 1 },
-    { 5120, 2 },
+    { 5120, 1 },
     { 6656, 4 },
     { 8192, 8 },
 };
